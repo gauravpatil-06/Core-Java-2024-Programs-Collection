bdf958d5eee157901f4df075cb481500baa9c1d2
Gaurav Patil <gp949958@gmail.com>
2024-03-09 09:50:33 +0530

    Added loop basics

---
+++ Control_Structures/Loops/for_loop.java
@@ -0,0 +1,12 @@
+class for_loop
+{
+    public static void main(String args[])
+    {
+        int i;
+
+        for (i = 1; i <= 5; i++)
+        {
+            System.out.println("Gaurav Patil");
+        }
+    }
+}
---
+++ Control_Structures/Loops/while_loop.java
@@ -0,0 +1,13 @@
+class while_loop
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+
+        while (i <= 5)
+        {
+            System.out.println("Gaurav Patil");
+            i++;
+        }
+    }
+}
