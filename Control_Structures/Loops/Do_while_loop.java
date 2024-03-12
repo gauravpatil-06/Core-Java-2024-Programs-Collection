cd85835ea7a38bcee0748bf44cef9a590d0f2341
Gaurav Patil <gp949958@gmail.com>
2024-03-12 14:15:55 +0530

    Added do-while and foreach

---
+++ Control_Structures/Loops/Do_while_loop.java
@@ -0,0 +1,14 @@
+class Do_while_loop
+{
+    public static void main(String args[])
+    {
+        int i = 1;
+
+        do
+        {
+            System.out.println("Gaurav Patil");
+            i++;
+        }
+        while (i <= 5);
+    }
+}
